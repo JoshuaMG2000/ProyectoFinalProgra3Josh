649a76b820781ea4d6d3646025833d40ef83dead
jgald <jgald@JOSH>
2025-05-15 11:59:18 -0600

    Primer Commit Chido Conectando Git a Netbeans

---
+++ src/main/java/com/jdmg/proyectofinalprogra3josh/ProyectoFinalProgra3Josh.java
@@ -0,0 +1,12 @@
+package com.jdmg.proyectofinalprogra3josh;
+
+/**
+ *
+ * @author jgald
+ */
+public class ProyectoFinalProgra3Josh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
